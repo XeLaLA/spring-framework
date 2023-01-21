121a8ac46f8497d396eb705176b9e7e4557f90de
XeLa <laxelaalexla@gmail.com>
2023-01-21 12:16:58 +0300

    Init commit

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,7 +1,5 @@
 package application;
 
-import config.DaoConfig;
-import config.ServicesConfig;
 import domain.Person;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,21 +9,15 @@ import org.springframework.stereotype.Component;
 import service.PersonService;
 import service.PersonServiceImpl;
 
-@ComponentScan()
+@ComponentScan(basePackages = {".dao",".service"})
 @Configuration
 public class Application {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(Application.class);
 
-        PersonService serviceOne = (PersonService) context.getBean("personeOne");
-        PersonService serviceTwo = (PersonService) context.getBean("personeTwo");
-
+        PersonService serviceOne = context.getBean(PersonService.class);
         Person ivan = serviceOne.getByName("Ivan");
-        Person petr = serviceTwo.getByName("Petr");
         System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
-        System.out.println("name: " + petr.getName() + " age: " + ivan.getAge());
-        System.out.println(serviceOne);
-        System.out.println(serviceTwo);
     }
 }
--- src/main/java/service/PersonServiceImpl.java
+++ src/main/java/service/PersonServiceImpl.java
@@ -2,21 +2,21 @@ package service;
 
 import dao.PersonDao;
 import domain.Person;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@Data
+@AllArgsConstructor
 public class PersonServiceImpl implements PersonService{
 
     private PersonDao dao;
 
-    @Autowired
-    public PersonServiceImpl(@Qualifier("PersoneOne") PersonDao dao) {
-        this.dao = dao;
-    }
 
     public Person getByName(String name) {
         return dao.findByName(name);
